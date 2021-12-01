5f86aca45b9247ef3a40f3c516e678d773a163ed
Jose Valdivia <78391806+lamula21@users.noreply.github.com>
2021-11-30 23:34:29 -0500

    Add files via upload

---
+++ src/CarPanel.java
@@ -0,0 +1,89 @@
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import javax.swing.JComponent;
+
+/**
+   This component draws two car shapes.
+*/
+public class CarPanel extends JComponent
+{  
+	private Car car1;
+	private int x,y, delay;
+	private CarQueue carQueue;
+	private int direction;
+	
+	CarPanel(int x1, int y1, int d, CarQueue queue)
+	{
+		delay = d;
+        x=x1;
+        y=y1;
+        car1 = new Car(x, y, this);
+        carQueue = queue;
+	}
+	public void startAnimation()
+	   {
+	      class AnimationRunnable implements Runnable
+	      {
+	         public void run()
+	         {
+	            try
+	            {
+	               for(int i=0;i<10;i++)
+	               {
+	            	   /** Adds 0,1,2 or 3 to queue
+	           	     *  0 = up
+	           	     *  1 = down
+	           	     *  2 = right
+	           	     *  3 = left
+	           	     */
+	            	   
+	            	   direction = carQueue.deleteQueue();
+	            	   switch (direction) {
+	            	   case 0: y -=10; break;
+	            	   			
+	            	   case 1: y += 10; break;
+	            	   			
+	            	   case 2: x += 10; break;
+	            	   case 3: x -= 10; break;
+	            	   }
+	            	   
+	            	   //The cars should go in the opposite direction if they hit a boundary.
+	            	   if (x < 0) {
+	            		   x += 10;
+	            	   }
+	            	   if (x > 300) {
+            			   x -= 10;
+            		   }
+	            	   else if( y < 0){
+	            		   y += 10;
+	            	   }
+	            	   else if (y> 400) {
+            			   y -= 10;}
+	            	   repaint();
+	            	   Thread.sleep(delay*1000);
+	            	   
+	               }
+	            }
+	            catch (InterruptedException exception)
+	            {
+	            	
+	            }
+	            finally
+	            {
+	            	
+	            }
+	         }
+	      }
+	      
+	      Runnable r = new AnimationRunnable();
+	      Thread t = new Thread(r);
+	      t.start();
+	   }
+	
+   public void paintComponent(Graphics g)
+   {  
+      Graphics2D g2 = (Graphics2D) g;
+
+      car1.draw(g2,x,y);    
+   }
+}
